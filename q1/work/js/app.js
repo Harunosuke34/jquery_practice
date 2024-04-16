9d8c928b2b2596386fba10b88e4aa5dd4859c3e6
原はるか <haru000034@gmail.com>
2024-04-16 20:58:46 +0900

    4月16日　jsコメントの整理

--- q1/work/js/app.js
+++ q1/work/js/app.js
@@ -4,19 +4,19 @@
 // ページが読み込まれた時の処理
 $(document).ready(function() {
 
-  // Q1-3 クリックしてフェードアウト
+  // Q1-3 idQ3をクリックしてフェードアウト
   $("#q3").click(function() {
     $(this).fadeOut(3000); // フェードアウトする時間は3秒
   });
 
-  // Q1-6 クリックして移動
+  // Q1-6 idQ6クリックして移動
   $("#q6").click(function() {
-    $(this).animate({ marginTop: "100px", marginLeft: "100px" }, 2000); // margin-topとmargin-leftを指定し、2秒かけて動く
+    $(this).animate({ marginTop: "100px", marginLeft: "100px" }, 2000); // margin-top100pxとmargin-left100pxを指定し、2秒かけて動く
   });
 
-  // Q1-9 対象の要素のindex番号を取得する処理
+  // Q1-9 idq9とliタグのindex番号を取得する処理
   $("#q9 li").click(function() {
-    var index = $(this).index(); // クリックされた要素のindex番号を取得
+    let index = $(this).index(); // idq9とliタグがクリックされた際にindex番号を取得
     alert("Index: " + index); // 取得したindex番号をアラートで表示
   });
 
